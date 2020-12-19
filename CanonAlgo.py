012dc01ed72ce59bd79b29ed97d36dbfec77aa17
bhavyajain768 <74612679+bhavyajain768@users.noreply.github.com>
2020-12-19 20:13:54 +0530

    Made function for doing col rotations

--- CanonAlgo.py
+++ CanonAlgo.py
@@ -15,6 +15,21 @@ def rowRotation(T,a,i,n):
 # This is to set new attributes values to the graph.
     networkx.set_node_attributes(T,D,'A')
 
+def colRotation(T,a,j,n):
+# Function for doing col rotation of j-th col a-times in the given Torus network of size (nxn)(a<n,j<n)
+    array,i = [],0
+    while(i<n):
+# This while loop is for storing the current attributes of nodes in an array before the rotation.
+        array[i] = T.nodes[(i,j)]['B']
+        i=i+1
+    i,D = 0,{}
+    while(i<n):
+# This while loop is for storing the new values of attribute values (after rotation) corresponding to (i,j) node in a dict.
+        D[((i-a+n)%n),j] = array[i]
+        i=i+1
+# This is to set new attributes values to the graph.
+    networkx.set_node_attributes(T,D,'B')
+
 def multiply(A,B):
 # A and B are 2-D arrays represnting two square matrices(nxn) and we have to multiply them using Canon's Algo.
     n = len(A)
